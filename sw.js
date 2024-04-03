70d9c5522e20c17d2c047987bc216f403dcaa7c5
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2024-04-03 01:54:17 -0500

    Attempt at PWA support

---
+++ sw.js
@@ -0,0 +1,22 @@
+var GHPATH = '/Toolkit';
+var APP_PREFIX = 'toolkit_';
+var VERSION = 'version_001';
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.md`,
+  `${GHPATH}/about.md`,
+  `${GHPATH}/bug-bounty.md`,
+  `${GHPATH}/ctfs-challenges.md`,
+  `${GHPATH}/learning-resources.md`,
+  `${GHPATH}/osint-services.md`,
+  `${GHPATH}/physical-security.md`,
+  `${GHPATH}/privacy-security.md`,
+  `${GHPATH}/useful-github-repos.md`,
+  `${GHPATH}/vuln-db.md`,
+  `${GHPATH}/_includes/head-custom.html`,
+  `${GHPATH}/_layouts/default.html`,
+  `${GHPATH}/assets/css/style.css`,
+  `${GHPATH}/assets/images/about.png`,
+  `${GHPATH}/_config.yml`,
+  `${GHPATH}/favicon.ico`,
+]
